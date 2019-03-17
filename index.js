3f5c3b133a05cbdd51c106218b2c9cb5c90c049f
Ahmed Elsayed <ahmedelsayed@Ahmeds-MacBook-Air-2.local>
2019-03-17 19:35:33 +0200

    up

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+
+  app.get('/places/:id', function (req, res) {
+    var id = req.params.id;
+    var p = req.params.p;
+    res.send(id+p)
+  })
+   
+  app.post('/places', function (req, res) {
+      var body = req.body;
+      res.send(body)
+  })
+   
+app.listen(3000);
