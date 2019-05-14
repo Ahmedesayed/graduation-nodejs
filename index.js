bbb882d78cf52eb3dd60732f121d79487466da2b
Ahmed Elsayed <ahmedelsayed@Ahmeds-MacBook-Air-9.local>
2019-05-14 21:35:51 +0200

    up

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
-var server = require('http').createServer(app);
-const io = require('socket.io')(server);
+// var server = require('http').createServer(app);
+var WebSocket = require('ws')
+var wss = new WebSocket.Server({ port: 8080 })
+app.listen(80, () => {
+    console.log('Welcome to our server at ' + 80);
+});
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Hello To Our Graduation Project Guest !')
 })
 
-io.on('connection', client => {
-    client.on('chat_direct', data => {
-        io.emit('message_sent',data);
-    });
-    client.on('disconnect', () => { console.log('disconnected') });
-});
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('ho!')
+})
 
-app.listen(process.env.PORT || 4000, () => {
-    console.log('Welcome to our server at ' + 4000);
-});
