fcf1a7951992044144068302420d64ac3161efd9
Ahmed Elsayed <ahmedelsayed@Ahmeds-MacBook-Air-9.local>
2019-05-14 04:10:48 +0200

    up

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
-var server = require('http').createServer(app);
-const io = require('socket.io')(server);
-var http = require("http")
-var url  = require("url")
+// var server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+var http, options, proxy, url;
 
-var proxy = url.parse(process.env.PROXIMO_URL)
+http = require("http");
 
-var  options ={ 
-    hostname: proxy.hostname,
-    port:     proxy.port || 80,
-    path:     "https://graduation-project12.herokuapp.com",
-    headers: {
-      "Proxy-Authorization": `Basic ${new Buffer(proxy.auth).toString("base64")}`
-    }
-}
+url = require("url");
 
-http.get(options,(res)=>{
-    console.log ("status code", res.statusCode)
-    console.log ("headers", res.headers)
-})
+proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
+target  = url.parse("http://ip.quotaguard.com/");
+console.log(target)
+options = {
+  hostname: proxy.hostname,
+  port: proxy.port || 80,
+  path: target.href,
+  headers: {
+    "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+    "Host" : target.hostname
+  }
+};
+
+http.get(options, function(res) {
+  res.pipe(process.stdout);
+  return console.log("status code", res.statusCode);
+});
 
 
 // app.use(bodyParser.json());
@@ -29,13 +34,13 @@ app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
-io.on('connection', client => {
-    console.log('connected with id ' + client.id)
-    client.on('chat_direct', data => {
-        io.emit('message_sent',data);
-    });
-    client.on('disconnect', () => { console.log('disconnected') });
-});
+// io.on('connection', client => {
+//     console.log('connected with id ' + client.id)
+//     client.on('chat_direct', data => {
+//         io.emit('message_sent',data);
+//     });
+//     client.on('disconnect', () => { console.log('disconnected') });
+// });
 
 app.listen(process.env.PORT || 4000, () => {
     console.log('Welcome to our server at ' + 4000);
