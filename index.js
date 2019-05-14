884d6f353c29aba20d91bdc17ecdf8a64e0810bd
Ahmed Elsayed <ahmedelsayed@Ahmeds-MacBook-Air-9.local>
2019-05-14 03:39:21 +0200

    up

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ var app = express();
 var bodyParser = require('body-parser')
 var server = require('http').createServer(app);
 const io = require('socket.io')(server);
+var http = require("http")
+var url  = require("url")
 
-server.listen(process.env.PORT || 4000, () => {
-    console.log('Welcome to our server at ' + 4000);
-});
+var proxy = url.parse(process.env.PROXIMO_URL)
 
-app.use(bodyParser.json());
+var  options ={ 
+    hostname: proxy.hostname,
+    port:     proxy.port || 80,
+    path:     "https://graduation-project12.herokuapp.com",
+    headers: {
+      "Proxy-Authorization": `Basic ${new Buffer(proxy.auth).toString("base64")}`
+    }
+}
+
+http.get(options,(res)=>{
+    console.log ("status code", res.statusCode)
+    console.log ("headers", res.headers)
+})
+
+
+// app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Hello World')
@@ -20,4 +35,8 @@ io.on('connection', client => {
         io.emit('message_sent',data);
     });
     client.on('disconnect', () => { console.log('disconnected') });
+});
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Welcome to our server at ' + 4000);
 });
