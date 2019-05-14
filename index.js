d39812cc0e6e35f9fd1a5d607e44b67ba7ebd163
Ahmed Elsayed <ahmedelsayed@Ahmeds-MacBook-Air-9.local>
2019-05-14 04:36:38 +0200

    up

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ http = require("http");
 url = require("url");
 
 proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
-target  = url.parse("https://graduation-project12.herokuapp.com/");
-console.log(target,proxy)
+target  = url.parse("http://ip.quotaguard.com/");
+console.log(target)
 options = {
   hostname: proxy.hostname,
   port: proxy.port || 80,
